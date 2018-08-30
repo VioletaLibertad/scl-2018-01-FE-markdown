93e9f8108e6eedf1d25737964836eb2eb6be57c8
VioletaLibertad <morada.libertad@gmail.com>
2018-08-30 17:32:40 -0300

    funciones de JS y test avanzando

--- index.js
+++ index.js
@@ -5,51 +5,5 @@ const path = require('path');
 /* Obtain what user wrote on CLI */
 const userInput = process.argv;
 let pathFromUser = userInput[2];
-console.log('prueba' + pathFromUser);
 mdLinks.obtainUserInput(pathFromUser);
 
-// console.log('probando a ver si funciona ' + absolutePath);
-
-/* Checking if file or directory */
-let stats = fs.statSync(absolutePath);
-if (stats.isDirectory() === true) {
-  fs.readdir(absolutePath, 'utf-8', (err, files) => {
-    if (err) throw err;
-    console.log('These files can be found here: ' + files);
-    mdFilesArray = files.filter((file) => path.extname(file) === '.md');
-    console.log('Array con archivos md ' + mdFilesArray);
-    return mdFilesArray;
-  });
-  console.log(mdFilesArray);
-} else if (stats.isFile() === true) {
-  if (path.extname(absolutePath) === '.md') {
-    singleMdFile = absolutePath;
-    console.log('This is a ' + singleMdFile);
-    return singleMdFile;
-  } 
-  console.log('Verificando extension del file ' + path.extname(absolutePath));
-}
-console.log(mdFilesArray);
-
-
-
-// console.log('is file? ' + stats.isFile());
-// console.log('is directory? ' + stats.isDirectory());
-
-
-
-
-
-// let result;
-
-// if (require.main === module) {
-//   for (let i = 0; i < userInput.length; i++) {
-//     result = (userInput[i]);
-//   }
-//   console.log(result);
-// }
-
-
-// process.argv.forEach((val, index) => {
-//   console.log(`${index}: ${val}`);
-// });
--- test/md-links.test.js
+++ test/md-links.test.js
@@ -4,11 +4,20 @@ describe('mdLinks module', () => {
   test('mdLinks to be an object', () => {
     expect(typeof mdLinks).toBe('object');
   });
-
 });
 
 describe('obtainUserInput function', () => {
   test('obtainUserInput to return an absolute path', () => {
     expect(mdLinks.obtainUserInput('./lib/md-links.js')).toBe('/home/violeta/Documentos/Laboratoria/md-links/scl-2018-01-FE-markdown/lib/md-links.js');
+  });
+});
+
+describe('pathIsFileOrDirectory function', () => {
+  test('pathIsFileOrDirectory should return an array if directory', () => {
+    expect.assertions(1);
+    return mdLinks.pathIsFileOrDirectory('/home/violeta/Documentos/Laboratoria/md-links/scl-2018-01-FE-markdown/assets')
+      .then((mdFilesArray) => {
+        expect(Array.isArray(mdFilesArray)).toBe(true);
+      });
   });
 });
